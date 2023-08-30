a84e7834a06caf67ff9fc950bc788fd896d934bd
Muhammad Rahim <muhammad.rahim@teledyne.com>
2023-08-29 17:16:31 -0700

    Minor modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resourceType, setResourceType] = useState('users')
+
+  const handleClick = (param) => {
+    console.log(param);
+    setResourceType(param);
+  }
 
   return (
     <>
-      <h1>Vite React App</h1>
-      <button>Users</button>
-      <button>Comments</button>
+      <h1>A Simple React App</h1>
+      <button onClick={()=>handleClick('users')}>Users</button>
+      <button onClick={()=>handleClick('comments')}>Comments</button>
+      <h2>Currently selected resource: {resourceType}</h2>
     </>
   )
 }
