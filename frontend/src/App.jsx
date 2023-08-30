3c4bc31040627b42beef0eb929cca31c5fb33af3
Muhammad Rahim <muhammad.rahim@teledyne.com>
2023-08-29 17:20:15 -0700

    Fetching data from json placeholder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [resourceType, setResourceType] = useState('users')
+  const [resourceType, setResourceType] = useState('users');
+  const [items, setItems] = useState([]);
 
   const handleClick = (param) => {
     console.log(param);
     setResourceType(param);
   }
+
+  useEffect(()=> {
+    const fetchData = async () => {
+      // Make the request
+      const result = await axios(
+        `https://jsonplaceholder.org/${resourceType}`,
+      );
+      // Update the items  
+      setItems(result.data);      
+    };
+
+    fetchData();
+  }, [resourceType])
 
   return (
     <>
@@ -15,6 +30,17 @@ function App() {
       <button onClick={()=>handleClick('users')}>Users</button>
       <button onClick={()=>handleClick('comments')}>Comments</button>
       <h2>Currently selected resource: {resourceType}</h2>
+      <ul>
+        {items.map(item => {
+              return <li key={item.id}>
+                <br />
+                {JSON.stringify(item)}    
+                <br />               
+                <button onClick={()=>handleDelete(item.id)}>Delete</button>
+                <hr />                
+              </li>                                              
+        })}
+       </ul>  
     </>
   )
 }
